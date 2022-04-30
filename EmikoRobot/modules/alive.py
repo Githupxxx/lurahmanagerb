206a83f389b1978b970b3f283b6439f8dca208bd
Githupxxx <104344083+Githupxxx@users.noreply.github.com>
2022-04-30 14:52:01 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/dac0ade914cf0bce37be6.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lurah Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), Aku lurah robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
   TEXT += f"⚪ **My Master : [cimoon](https://t.me/Zyrexcim)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**matur nuwun kanggo nambah kulo ❤️**"
+  TEXT += "**matur nuwun kanggo nambah ne kulo ❤️**"
   BUTTON = [[Button.url("Help", "https://t.me/mbahlurahubot?start=help"), Button.url("Support", "https://t.me/CIRCLEPOKE")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
